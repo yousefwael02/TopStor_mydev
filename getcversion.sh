4fa701acfb4b1b3e6a289d5610998852fc76e8c9
sdlfkj <dkdjkl@dljkf.com>
2023-05-06 11:58:17 +0300

    fixing

--- getcversion.sh
+++ getcversion.sh
@@ -4,15 +4,21 @@ echo $@ > /root/getcversion
 leaderip=`echo $@ | awk '{print $1}'`
 leader=`echo $@ | awk '{print $2}'`
 myhost=`echo $@ | awk '{print $3}'`
+checksync=`echo $@ | awk '{print $4}'`
 version=`git branch | grep '*' | awk '{print $2}'`
+
 commit=`git show --abbrev-commit | grep commit | head -1 | awk '{print $2}'`
-oldversion=`/TopStor/etcdget.py $leaderip cversion/$myhost`
-echo $oldversion | grep $version-$commit
+echo h$checksync | grep hchecksync
 if [ $? -ne 0 ];
 then
-	echo asking to sync
-	stamp=`date +%s`
-	./etcdput.py $leaderip cversion/$myhost $version-$commit
-	./etcdput.py $leaderip sync/cversion/_____/request cversion_$stamp 
-	./etcdput.py $leaderip sync/cversion/_____/request/$leader cversion_$stamp 
+	oldversion=`/TopStor/etcdget.py $leaderip cversion/$myhost`
+	echo $oldversion | grep $version-$commit
+	if [ $? -ne 0 ];
+	then
+		echo asking to sync
+		stamp=`date +%s`
+		./etcdput.py $leaderip cversion/$myhost $version-$commit
+		./etcdput.py $leaderip sync/cversion/_____/request/$leader cversion_$stamp 
+		./etcdput.py $leaderip sync/cversion/_____/request cversion_$stamp 
+	fi
 fi
