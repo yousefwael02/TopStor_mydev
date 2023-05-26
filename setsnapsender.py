b0b788cf54a83743348aaef739150d00cc88e918
sdlfkj <dkdjkl@dljkf.com>
2023-05-26 13:27:59 +0300

    fixing

--- setsnapsender.py
+++ setsnapsender.py
@@ -12,6 +12,8 @@ def setsnapshotsender(snapshot,cip):
     cmdline='docker exec etcdclient /TopStor/etcdgetlocal.py leaderip'
     leaderip=subprocess.run(cmdline.split(),stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n','').replace(' ','')
     sender = get(leaderip,'Partner', cip)[0][0].split('/')[1].split('_')[0]
+    cmd = 'zfs set partner:sender='+sender+' '+fullname    
+    result = subprocess.run(cmd.split(),stdout=subprocess.PIPE).stdout.decode().split()
     print('sender', sender)
     
 
