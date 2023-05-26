7f5b2ddf5e7f22e0061325628208f70cbd446d87
sdlfkj <dkdjkl@dljkf.com>
2023-05-26 13:24:01 +0300

    fixing

--- setsnapsender.py
+++ setsnapsender.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys, subprocess
+from etcdget import etcdget as get
 
 leaderip = '0'
 def setsnapshotsender(snapshot,cip):
     cmd = 'zfs list -t snapshot'    
-    result = subprocess.run(cmdline.split(),stdout=subprocess.PIPE).stdout.decode() 
+    result = subprocess.run(cmd.split(),stdout=subprocess.PIPE).stdout.decode() 
     fullname = [ x.split('\t')[0] for x in result if snapshot in x ][0]
     print('fullname',fullname)
     cmdline='docker exec etcdclient /TopStor/etcdgetlocal.py leaderip'
