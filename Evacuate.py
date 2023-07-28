7e24fb104749c3caa48ffac1e172397220cc3237
sdlfkj <dkdjkl@dljkf.com>
2023-07-29 02:52:59 +0300

    fixing

--- Evacuate.py
+++ Evacuate.py
@@ -52,7 +52,7 @@ def do(leaderip,myhost, *args):
     put(leaderip, 'ActivePartners/dhcpEvacuate.'+args[-2],'0')
     setall(leaderip, myhost,args[-2],evacip,args[-1])
     put(leaderip, 'sync/evacuatehost/syncfn_setall_'+args[-2]+'_'+args[-1]+'/request', 'evacuatehost_'+str(stamp))
-    put(leaderip, 'sync/evacuatehost/syncfn_setall_'+args[-2]+'_'+args[-1]+'/request/'+myhost, 'evacuatehost_'+str(stamp))
+    #put(leaderip, 'sync/evacuatehost/syncfn_setall_'+args[-2]+'_'+args[-1]+'/request/'+myhost, 'evacuatehost_'+str(stamp))
     dels(discip,'possible', args[-2])
 
         
