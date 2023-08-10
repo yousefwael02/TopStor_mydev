a4451eb21d78bfc9ee02dcc8b9cdec6e784d728d
sdlfkj <dkdjkl@dljkf.com>
2023-08-10 19:44:44 +0300

    fixing

--- systempull.sh
+++ systempull.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/sh
 fnupdate () {
 	echo '###########################################' $1
-	git checkout QSD3.15
-	git branch -D $1
 	git fetch origin $1
 	if [ $? -ne 0 ];
 	then
 		echo something went wrong while updating $1 .... consult the devleloper
 		exit
 	fi
+	git branch -D tempb
+	git checkout -- *
+	git rm -rf __py*
+	rm -rf __py*
+	git checkout -b tempb
+	git branch -D $1
 	git checkout -b $1 origin/$1
 	git reset --hard
 	git checkout -- *
 	git rm -rf __py*
 	rm -rf __py*
-	#git add --all
-	#git commit -am 'fixing' 
 	sync
 	sync
 	sync
