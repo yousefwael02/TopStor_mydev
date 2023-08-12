0f02bdd9aa441c9c102648b989db1b4d43ef1c4d
sdlfkj <dkdjkl@dljkf.com>
2023-08-12 10:52:54 +0300

    fixig

--- nodeinfo.sh
+++ nodeinfo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+leaderip=`echo $@ | awk '{print $1}'`
 res=1
 while [ $res -le 5 ];
 do
-	noden=$(docker exec etcdclient /TopStor/etcdgetlocal.py clusternode)
+	noden=`/TopStor/etcdget.py $leaderip clusternode`
 	res=$(echo $noden | wc -c)
 	
 done
 res=1
 while [ $res -le 5 ];
 do
-	nodei=$(docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip)
+	nodei=`/TopStor/etcdget.py $leaderip clusternodeip`
 	res=$(echo $nodei | wc -c)
-	replinextport=$(docker exec etcdclient /TopStor/etcdgetlocal.py replinextport)
 done
 res=1
-while [ $res -le 5 ];
+while [ $res -lt 2 ];
 do
-	replinextport=$(docker exec etcdclient /TopStor/etcdgetlocal.py replinextport)
+	replinextport=`/TopStor/etcdget.py $leaderip replinextport`
 	res=$(echo $replinextport | wc -c)
 	
 done
