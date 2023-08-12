bd105c04d9a119ca353f2090a105b780c899176a
sdlfkj <dkdjkl@dljkf.com>
2023-08-12 07:02:44 +0300

    fixing

--- nodeinfo.sh
+++ nodeinfo.sh
@@ -2,14 +2,14 @@
 res=1
 while [ $res -le 5 ];
 do
-	noden=`docker exec etcdclient /TopStor/etcdgetlocal.py clusternode`
+	noden=$(docker exec etcdclient /TopStor/etcdgetlocal.py clusternode)
 	res=$(echo $noden | wc -c)
 	
 done
 res=1
 while [ $res -le 5 ];
 do
-	nodei=`docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip`
+	nodei=$(docker exec etcdclient /TopStor/etcdgetlocal.py clusternodeip)
 	res=$(echo $nodei | wc -c)
 done
 echo _result_${noden}_${nodei}_
