7feeb7c607bcbe3159bffa0944092c5827599a61
sdlfkj <dkdjkl@dljkf.com>
2023-10-07 21:04:09 +0300

    fixing

--- docker_setup.sh
+++ docker_setup.sh
@@ -556,5 +556,11 @@ then
 	echo running httpd fowrarder as I am not primary
 	docker run --rm --name httpd --hostname shttpd --net bridge0 -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $myclusterip:19999:19999 -p $myclusterip:80:80 -p $myclusterip:443:443 -v $shttpdf:/usr/local/apache2/conf/httpd.conf -v /root/topstorwebetc:/usr/local/apache2/topstorwebetc -v /topstorweb:/usr/local/apache2/htdocs/ -itd moataznegm/quickstor:git
 	docker run -itd --rm --name flask --hostname apisrv -v /etc/localtime:/etc/localtime:ro -v /pace/:/pace -v /pacedata/:/pacedata/ -v /root/gitrepo/resolv.conf:/etc/resolv.conf --net bridge0 -p $myclusterip:5001:5001 -v /TopStor/:/TopStor -v /TopStordata/:/TopStordata moataznegm/quickstor:flask
+ #docker run -d -p $myclusterip:9090:9090 -v /TopStordata/prom/:/prometheus -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --name promserver prom/prometheus
+ docker start promserver
 fi
+#docker run -d -p $mynodeip:9100:9100 -v /proc:/proc -v /sys:/sys --name promexport prom/node-exporter
+docker start promexport
+#docker run -d -p $myclusterip:4000:3000 -v /TopStordata/promgraf:/var/lib/grafana --name promgraf grafana/grafana 
+docker start promgraf
  /pace/fapilooper.sh & disown
