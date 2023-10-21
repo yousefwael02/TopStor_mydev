c0d8f09f012ccd42669b6a5d3cdeca3b6757d572
sdlfkj <dkdjkl@dljkf.com>
2023-10-21 08:35:49 +0300

    fixing

--- promrepli.sh
+++ promrepli.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 cd /TopStor/
 leaderip=`echo $@ | awk '{print $1}'`
+myhostip=`echo $@ | awk '{print $2}'`
 rm -rf /prom/prom.yml
 cp /TopStor/promrepli.yml  /prom/promyaml
 sed -i "s/LEADERIP/$leaderip/g" /prom/promyaml
 cat /prom/promyaml >> /prom/prom.yml
 rm -rf /prom/promyaml
-docker ps | grep  promserver
-if [ $? -eq 0 ];
-then
-	docker restart promserver
-	docker restart promgraf
-else
-	docker run -d -p $leaderip:9090:9090 -v /TopStordata/prom/prom.yml:/etc/prometheus/prometheus.yml -v /TopStordata/prom/:/prometheus -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --name promserver prom/prometheus
+docker rm -f promserver
+docker rm -f promgraf
+	docker run -d -p $myhostip:9090:9090 -v /prom/prom.yml:/etc/prometheus/prometheus.yml -v /prom/:/prometheus -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --name promserver prom/prometheus
  	rm -rf /promgraf/hosts
  	cp /TopStor/promgrafhosts /promgraf/hosts
- 	sed -i "s/MYCLUSTER/$leaderip/g" /promgraf/hosts 
- 	docker run -d -p $leaderip:4000:3000 -v /promgraf/grafana.ini:/etc/grafana/grafana.ini -v /promgraf:/var/lib/grafana -v /promgraf/hosts:/etc/hosts --name promgraf grafana/grafana
-fi
+ 	sed -i "s/MYCLUSTER/$myhostip/g" /promgraf/hosts 
+ 	docker run -d -p $myhostip:4000:3000 -v /promgraf/grafana.ini:/etc/grafana/grafana.ini -v /promgraf:/var/lib/grafana -v /promgraf/hosts:/etc/hosts --name promgraf grafana/grafana
--- promserver.sh
+++ promserver.sh
@@ -19,15 +19,10 @@ for port in "${ports[@]}"; do
 done
 cat /prom/promyaml >> /prom/prom.yml
 rm -rf /prom/promyaml
-docker ps | grep  promserver
-if [ $? -eq 0 ];
-then
-	docker restart promserver
-	docker restart promgraf
-else
-	docker run -d -p $leaderip:9090:9090 -v /TopStordata/prom/prom.yml:/etc/prometheus/prometheus.yml -v /TopStordata/prom/:/prometheus -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --name promserver prom/prometheus
+docker rm -rf promserver
+docker rm -rf promgraf
+	docker run -d -p $leaderip:9090:9090 -v /prom/prom.yml:/etc/prometheus/prometheus.yml -v /prom/:/prometheus -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --name promserver prom/prometheus
  	rm -rf /promgraf/hosts
  	cp /TopStor/promgrafhosts /promgraf/hosts
  	sed -i "s/MYCLUSTER/$leaderip/g" /promgraf/hosts 
  	docker run -d -p $leaderip:4000:3000 -v /promgraf/grafana.ini:/etc/grafana/grafana.ini -v /promgraf:/var/lib/grafana -v /promgraf/hosts:/etc/hosts --name promgraf grafana/grafana
-fi
