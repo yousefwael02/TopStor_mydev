cc49d625aa0e7f7c08cd9350026544bd0ca1f906
sdlfkj <dkdjkl@dljkf.com>
2024-01-27 19:57:48 +0300

    fixing

--- replicatenow.py
+++ replicatenow.py
@@ -19,6 +19,25 @@ nodeloc = ''
 replitype = 'Receiver'
 isitopen = 'closed'
 
+def oldgetnodeloc(receiver,ownerip='ssh'):
+    cmd = 'ps -ef'
+    receiverconn = subprocess.run(cmd.split(' '),stdout=subprocess.PIPE).stdout.decode().split('\n')
+    try:
+        receiverconn = [ rec for rec in receiverconn if {'ssh'+receiver,'Rremote',ownerip}.issubset(rec) ][0].split('ssh')[1].split('-N')[0]
+        host = receiverconn.split('/')[-1]
+        return 'ssh'+receiverconn+host
+    except:
+        return 'not available'
+def getnodeloc(receiver):
+    cmd = 'ps -ef'
+    receiverconn = subprocess.run(cmd.split(' '),stdout=subprocess.PIPE).stdout.decode().split('\n')
+    if len(receiverconn)< 10:
+        return 'not available'
+    receiverconn = [ rec for rec in receiverconn if 'ssh'+receiver in rec and 'Rremote' in rec ][0].split('ssh')[1].split('-N')[0]
+    host = receiverconn.split('/')[-1]
+    
+    return 'ssh'+receiverconn+host
+
 def dosync(sync,  *args):
   global leaderip, leader
   dels(leaderip, 'pullsync',sync) 
@@ -65,7 +84,7 @@ def createnodeloc(receiver, cmd, userreq):
  return nodeloc
  
 
-def replistream(receiver, nodeip, snapshot, nodeowner, poolvol, pool, volume, csnaps):
+def replistream(receiver, snapshot, nodeowner, poolvol, pool, volume, csnaps):
  global allinfo, phrase, myclusterip, pport, nodeloc, replitype, leaderip, etcdip
  myvol = pool+'/'+volume 
  mysnaps = sorted(allinfo['volumes'][volume]['snapshots'], key= lambda x : x.split('.')[-1], reverse=True)
@@ -96,25 +115,16 @@ def replistream(receiver, nodeip, snapshot, nodeowner, poolvol, pool, volume, cs
     extras = ''
  quota=subprocess.run(cmd.split(' '),stdout=subprocess.PIPE).stdout.decode().split('\t')[2]
  oldsnap = 'noold'
- nodeloccmd = nodeloc + ' /TopStor/getlatestsnap.sh '+volume
- try:
-  isopen, result = checkpartner(nodeloccmd)
- except:
-   print('result_failresult_ connection to the remote parnter')
-   exit()
-
+ nodeloccmd = nodeloc + '/TopStor/getlatestsnap.sh '+volume
+ result = subprocess.run(nodeloccmd.split(' '),stdout=subprocess.PIPE).stdout.decode()
  remotesnap = result.split('result_')
  if remotesnap != 'noold':
   cmd = 'zfs list -t snapshot'
   mysnaps = subprocess.run(cmd.split(' '),stdout=subprocess.PIPE).stdout.decode()
   if remotesnap[1] in mysnaps:
    oldsnap = remotesnap[2] 
- nodeloccmd = nodeloc + ' /TopStor/targetcreatevol.sh '+poolvol+' '+volip+' '+volsubnet+' '+quota+' '+voltype+' '+' '+oldsnap+' '+volgrps+' '+extras
- isopen, result = checkpartner(nodeloccmd)
- if 'open' not in isopen:
-   print('result_failresult_ connection to the remote parnter')
-   exit()
-
+ nodeloccmd = nodeloc + '/TopStor/targetcreatevol.sh '+poolvol+' '+volip+' '+volsubnet+' '+quota+' '+voltype+' '+' '+oldsnap+' '+volgrps+' '+extras
+ result = subprocess.run(nodeloccmd.split(' '),stdout=subprocess.PIPE).stdout.decode()
  response = result.split('result_')
  print('the response of create:',response)
  if oldsnap == 'noold':
@@ -146,21 +156,14 @@ def replistream(receiver, nodeip, snapshot, nodeowner, poolvol, pool, volume, cs
    continue
   if snap not in mysnaps:
    destroy = destroy + poolvol+'@'+snap+',' 
- if len(destroy) > 5:
+ if len(destroy) > 5 and 'noold' not in destroy[:-1]:
+  print('destorying',destroy[:-1])
   nodeloccmd = nodeloc + ' /TopStor/zfsdestroy.sh '+destroy[:-1]
   with open('/root/destroynow','w') as f:
     f.write(cmd+'\n')
-  isopen , _ = checkpartner(nodeloccmd)
-  if 'open' not in isopen:
-   print('result_failresult_ connection to the remote parnter')
-   exit()
-
- 
- nodeloccmd = '/usr/sbin/zfs list -H -t snapshot -o name'
- isopen , snaps = checkpartner(nodeloccmd)
- if 'open' not in isopen:
-  print('result_failresult_ connection to the remote parnter')
-  exit()
+  result = subprocess.run(nodeloccmd.split(' '),stdout=subprocess.PIPE).stdout.decode()
+ nodeloccmd = nodeloc +  '/usr/sbin/zfs list -H -t snapshot -o name'
+ snaps = subprocess.run(nodeloccmd.split(' '),stdout=subprocess.PIPE).stdout.decode()
  print('-----------------------------------')
  print('end checking csnaps', snaps)
  print('-----------------------------------')
@@ -186,7 +189,7 @@ def repliparam(snapshot, receiver, userreq='system'):
  volquota = str(allinfo['volumes'][volume]['quota'])
  snapused = '0' 
  cmd = ' /TopStor/repliSelection.py '+volume+' '+volquota+' '+snapshot
- nodeloc = createnodeloc(receiver, cmd, userreq)
+ nodeloc = getnodeloc(receiver)
  cmd = nodeloc + cmd
  finalresponse = subprocess.run(cmd.split(' '),stdout=subprocess.PIPE).stdout.decode().split('result_')[1]
  print('finalresponse',finalresponse)
@@ -198,12 +201,16 @@ def repliparam(snapshot, receiver, userreq='system'):
   return 'No_Sppue'
  print('selection',finalresponse)
  nodeowner = finalresponse.split(':')[0]
+ nodehost = nodeloc.split(' ')[-2]
+ print('old',nodeloc)
+ print('nodeowner:',nodeowner, nodehost)
+ nodeloc = nodeloc.replace(nodehost,nodeowner) 
  poolvol = finalresponse.split(':')[1].split('@')[0]
  try:
   csnaps = finalresonse.split('@')[1]
  except:
   csnaps = 'noold'
- result = replistream(receiver, nodeip, snapshot, nodeowner, poolvol, pool, volume, csnaps)
+ result = replistream(receiver, snapshot, nodeowner, poolvol, pool, volume, csnaps)
  if 'fail' in result:
   print('fail')
   cmd = '/usr/sbin/zfs destroy -r '+' '+pool+'/'+volume+'@'+snapshot 
