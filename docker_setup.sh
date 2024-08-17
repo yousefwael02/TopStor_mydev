e4b87310dfa27a31703e39cd307ae31370e1536a
sdlfkj <dkdjkl@dljkf.com>
2024-08-17 06:21:59 +0300

    fixing

--- docker_setup.sh
+++ docker_setup.sh
@@ -596,5 +596,5 @@ docker rm -f promexport
 docker run -d -p $mynodeip:9100:9100 -v /proc:/proc -v /sys:/sys --name promexport prom/node-exporter
 docker rm -f promcadvisor
 docker run   --volume=/:/rootfs:ro   --volume=/var/run:/var/run:ro   --volume=/sys:/sys:ro   --volume=/var/lib/docker/:/var/lib/docker:ro   --volume=/dev/disk/:/dev/disk:ro   --publish=$mynodeip:9101:8080   --detach=true   --name=promcadvisor   --privileged   --device=/dev/kmsg   gcr.io/cadvisor/cadvisor
- /pace/zfsping.py $leaderip $myhost & disown
+ #/pace/zfsping.py $leaderip $myhost & disown #### it is in refreshdisown
  /pace/fapilooper.sh & disown
