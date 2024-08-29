8618573e46d50aae20723a728ccf96fa7084a6d0
sdlfkj <dkdjkl@dljkf.com>
2024-08-29 19:34:41 +0300

    fixing

--- fastselect.py
+++ fastselect.py
@@ -13,6 +13,7 @@ disksinfo = {}
 count = 0 
 mindisksize = 0 
 diskscat = {}
+mustinclude = ''
 
 def norm(val):
  units={'B':1/1024**2,'K':1/1024, 'M': 1, 'G':1024 , 'T': 1024**2 }
@@ -26,153 +27,6 @@ def norm(val):
   else:
    return float(val[:-1])*float(units['B'])
 
-def operate(df1row,df2row):
- res = []
- for i in df1row:
-    if len(i) < 1:
-        continue
-    try:
-        for k in df2row:
-            res.append(feature_calc(i,k))
-    except:
-        continue
- return res   
-
-def feature_calc(i, k):
-    global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude,diskscat, redundancy
-    lendisks = len(disksinfo)
-    combineddisks = i[0]+','+k[0]
-    toprint = 0 
-    res = []
-    if i[3] not in k[3]:
-        combinedhosts = i[3]+','+k[3]
-    else:
-        combinedhosts = k[3]
-    if  i[4] >= k[4] or  i[2] == float('inf') or k[2] == float('inf'): # less matrix calcs
-        if toprint:
-            print('iam here 1')
-        res =  [i[0]+','+k[0],i[1],float('inf'),combinedhosts]
-         
-    else:
-        if toprint:
-            print('iam here 5')
-        if i[1] < lendisks+1:  # disk hosts test
-            disksn = combineddisks.count(',')+1
-            hostsn = combinedhosts.count(',')+1
-            if toprint:
-                print('iam here 6')
-            if hostsn < disksn:
-                if toprint:
-                    print('iam here 7')
-                if ',' not in k[0]:
-                    res =  [combineddisks,i[1],lendisks,combinedhosts]
-                    if toprint:
-                        print('iam here 8')
-                else: 
-                    res =  [combineddisks,i[1],k[2]+lendisks,combinedhosts]
-                    if toprint:
-                        print('iam here 9')
-            else:
-                    res =  [combineddisks,i[1],0,combinedhosts] # hosts equal or higher than the disks so still 0
-                    if toprint:
-                        print('iam here 10')
-        elif i[1] < (2*lendisks)+1: # disk type
-            if toprint:
-               print('iam here 11')
-            disktypes.add(0)  # let all disktypes be 0 till we change them later
-            if ',' not in k[0]:
-                disktypes.add(0)
-                if toprint:
-                    print('iam here 12')
-            if len(disktypes) > 1:
-                  res =  [i[0]+','+k[0],i[1],float('inf'),combinedhosts]
-                  if toprint:
-                    print('iam here 13')
-            else:
-                  res =  [i[0]+','+k[0],i[1],0,combinedhosts]
-                  if toprint:
-                    print('iam here 14')
-        elif i[1] < (3*lendisks)+1: # disk sizes test 
-            if toprint:
-               print('iam here 15')
-            if diskscat[i[0]]['size'] == mindisksize: 
-                if toprint:
-                    print('iam here 16')
-                if ',' not in k[0] and diskscat[k[0]]['size'] == mindisksize:
-                    res =  [i[0]+','+k[0],i[1],0,combinedhosts]
-                if toprint:
-                    print('iam here 17')
-                elif ',' not in k[0] and diskscat[k[0]]['size'] > mindisksize:
-                    if toprint:
-                        print('iam here 18')
-                    res =  [i[0]+','+k[0],i[1],1,combinedhosts]
-                elif ',' in k[0]:
-                    if toprint:
-                        print('iam here 19')
-                    res =  [i[0]+','+k[0],i[1],k[2],combinedhosts]
-            elif diskscat[i[0]]['size'] > mindisksize:
-                if toprint:
-                    print('iam here 20')
-                if ',' not in k[0] and diskscat[k[0]]['size'] == mindisksize:
-                    res =  [i[0]+','+k[0],i[1],1,combinedhosts]
-                    if toprint:
-                        print('iam here 21')
-                elif ',' not in k[0] and diskscat[k[0]]['size'] > mindisksize:   # both disks are higher than minimum -- it is not allowed
-                    if toprint:
-                        print('iam here 22')
-                    if count  == 2:
-                        res =  [i[0]+','+k[0],i[1],float('inf'),combinedhosts]
-                        if toprint:
-                            print('iam here 23')
-                    else: 
-                        res =  [i[0]+','+k[0],i[1],2,combinedhosts]
-                        if toprint:
-                            print('iam here 24')
-
-                elif ',' in k[0] and k[2]+1 < count :
-                    res =  [i[0]+','+k[0],i[1],k[2]+1,combinedhosts]
-                    if toprint:
-                        print('iam here 25')
-                else:
-                    res =  [i[0]+','+k[0],i[1],float('inf'),combinedhosts]
-                    if toprint:
-                        print('iam here 26')
-            else:
-                if toprint:
-                    print('iam here 29')
-                res =  [i[0]+','+k[0],i[1],float('inf'),combinedhosts]
-    if len(res) < 3:
-        if toprint:
-           print('iam here 30')
-        return []
-    if toprint:
-        print('iam here 31')
-    res = res + [i[4]]
-    if res[0] not in combinations:
-        if toprint:
-            print('iam here 32')
-        combinations[res[0]] = res[2]
-        if toprint:
-            print('iam here 33')
-            print('comb0',combinations[res[0]])
-    else:
-        if toprint:
-            print('iam here 34')
-            print('comb1',combinations[res[0]])
-        combinations[res[0]] = combinations[res[0]]+res[2]
-        if toprint: 
-            print('iam here 35')
-            print('comb2',combinations[res[0]])
-    if toprint:
-        print('iam here 36')
-        printlist = [{x:combinations[x]} for x in combinations if combinations[x] != float('inf')]
-        print('still feasible',printlist)
-    return res   
-                
-def combine_features(column_values):
-    global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude, diskscat
-    ccolumn_values = column_values.tolist()
-    return (ccolumn_values[0][0],ccolumn_values[0][1],float(ccolumn_values[0][2])+float(ccolumn_values[1][2])+float(ccolumn_values[2][2]))
 
 def calc_size(comb):
     sizepenalty = 0
@@ -242,55 +96,16 @@ def veryfastselect(elements):
         f.write(",".join(best_disk)+'\n')
     return ",".join(best_disk)
 
-
-
-
-def fastdiskselect(elements):
-    global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude
-    df = pd.DataFrame(elements)
-
-    result_df = df
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    for c in range(1,count):
-        result_data = [operate(row1, row2) for row1, row2 in zip(df.values, result_df.values)]
-        result_df = pd.DataFrame(result_data) 
-    if count == 1:
-        result_data = [operate(row1, row2) for row1, row2 in zip(df.values, result_df.values)]
-        result_df = pd.DataFrame(result_data) 
-    keytodel = set()
-    print(combinations)
-    for key in combinations:
-         if(key.count(',')+1 <  count):
-            keytodel.add(key)
-    for key in keytodel:
-         del combinations[key]
-    best_value = min(combinations.items(), key=lambda x: x[1])[1]
-    best_disk = [x for x in combinations.items() if x[1] == best_value and x[1] != float('inf')]
-    print('---------------------------------------------------')
-    print('diskscat',len(diskscat),diskscat.keys())
-    print('computed combinations',len(combinations))
-    print('to delete combinations', len(keytodel))
-    print('number of best disk combinations',len(best_disk))
-    print('the best disk list', best_disk)
-    print('---------------------------------------------------')
-    combinations = dict()
-    hosts = set()
-    mindisksize = 0
-    count = 0 
-    mindisksize = 0 
-    mustinclude = ''
-    return best_disk
-
-def optimizedisks(leaderip, fraid, fdisksinfo):
+def optimizedisks(leaderip, fraid, fdisksinfo,excludelst=''):
     global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude, redundancy, diskscat
     raid, disksinfo = fraid, fdisksinfo
     with open('/TopStordata/fastselectopt','w') as f:
         f.write(str(fraid)+'\n'+str(fdisksinfo)+'\n')
     count = len(raid['disklist'])
     mindisksize = min([ norm(x['size']) for x in raid['disklist'] if x['changeop'] in ['ONLINE']])
-    mustinclude = ''
     for disk in raid['disklist']:
+        if 'dm-' in disk['name']:
+            continue
         if len(mustinclude) == 0:
             mustinclude = disk['name']
         else:
@@ -298,19 +113,18 @@ def optimizedisks(leaderip, fraid, fdisksinfo):
     if 'mirror' in raid['name'] and count == 1:
         count = 2
     disks = {'diskcount':count , 'disk': mindisksize }
-    print('mustinclude', mustinclude)
-    #with open('/TopStordata/fastselectopt','w') as f:
-    #    f.write(str(count)+' '+str(mindisksize)+' '+str(disks)+' '+str(mustinclude)+'\n')
     combinations = dict()
     hosts = set()
-    #disktypes = set()
-    #disks = {}
-    #disksinfo = {}
-    #count = 0 
-    #mindisksize = 0 
-    return selectdisks(leaderip, disks, disksinfo)
+    selectthedisks = selectdisks(leaderip,disks,disksinfo,'',excludelst)
+    mustinclude=''
+    disktypes = set()
+    disks = {}
+    disksinfo = {}
+    count = 0 
+    mindisksize = 0 
+    return selectthedisks 
 
-def featuring(leaderip):
+def featuring(leaderip,excludelst=''):
     global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude, diskscat
     counter = 1
     print('current', disks)
@@ -324,7 +138,7 @@ def featuring(leaderip):
     needtoreplacelst = get(leaderip,'needto','--prefix')
     needtoreplace = str(needtoreplacelst)
     for disk in disksinfo:
-        if norm(disksinfo[disk]['size']) < norm(disks['disk']) or disksinfo[disk]['changeop'] not in [ 'free', 'ONLINE' ] or ( disksinfo[disk]['changeop'] in 'ONLINE' and disk not in mustinclude) or disk in needtoreplace:
+        if norm(disksinfo[disk]['size']) < norm(disks['disk']) or disksinfo[disk]['changeop'] not in [ 'free', 'ONLINE' ] or ( disksinfo[disk]['changeop'] in 'ONLINE' and disk not in mustinclude) or disk in needtoreplace or disk in excludelst:
             continue
         else:
             key = f"{disksinfo[disk]['host']}_{disksinfo[disk]['size']}"
@@ -339,35 +153,28 @@ def featuring(leaderip):
                 feature2.append([key,lendisks+counter,0,disksinfo[disk]['host'],counter])
                 feature3.append([key,(2*lendisks)+counter,0,disksinfo[disk]['host'],counter])
                 counter +=1
-            diskscat[key]['disks'].append(disk)
+            if disk in mustinclude:
+                diskscat[key]['disks'] = [disk] + diskscat[key]['disks']
+            else:
+                diskscat[key]['disks'].append(disk)
             diskscat[key]['diskcount'] += 1
-    
     elements.append(feature1)
     elements.append(feature2)
     elements.append(feature3)
     return elements 
 
 
-def selectdisks(leaderip, fdisks,fdisksinfo,addtopool=''):
+def selectdisks(leaderip, fdisks,fdisksinfo,addtopool='',excludelst=''):
     global hosts, disktypes, mindisksize, count, combinations, disks, disksinfo, mustinclude, diskscat, redundancy
     disks, disksinfo = fdisks, fdisksinfo
     with open('/TopStordata/fastselect','w') as f:
         f.write('fdisks: '+str(fdisks.keys())+'\n'+str(fdisks)+'\n'+str(fdisksinfo)+'/n')
     count = disks['diskcount']
     hosts = set()
-    mustinclude = ''
     print('count',count)
-    #if count ==1:
-    #    count = disks['diskcount'] + 1
-    #if len(addtopool) > 0 :
-    #    pooldisks = [x for x in disksinfo if addtopool in disksinfo[x]['pool']]
-    #    mustinclude = ','.join(pooldisks)
-    ####################################
-    ### need to optimize in case of adding single disks to the single .. no parity.. 
-    ######### it shoudl return non-inf
     mindisksize = disks['disk'] 
     elements = []
-    elements = featuring(leaderip)
+    elements = featuring(leaderip,excludelst)
     combinations = dict()
     res = veryfastselect(elements)
     disksinfo = []
