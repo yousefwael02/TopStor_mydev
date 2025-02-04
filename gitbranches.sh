c84422fdcf87d94d715df9086205f01c466305b9
sdlfkj <dkdjkl@dljkf.com>
2025-02-04 15:20:35 +0300

    fixing

--- gitbranches.sh
+++ gitbranches.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git for-each-ref --sort=-creatordate --format '%(refname:short) %(creatordate:short) %(objectname:short)' refs/heads/ | while read -r branch date commit; do
-    echo "$branch,$date,$commit"
+    echo "$branch, $date, $commit"
 done
