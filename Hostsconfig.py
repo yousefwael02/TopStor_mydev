53f96af0ea9388c89f29ea7276343dbb1714e0dd
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-11-25 14:11:17 +0200

    Update Hostsconfig.py

--- Hostsconfig.py
+++ Hostsconfig.py
@@ -26,6 +26,37 @@ def getall(*bargs):
   dnssearch = get('dnssearch/'+hostname)[0]
   alias = get('alias/'+hostname)[0]
   ports = get2(leaderip,'ports/'+hostname,'--prefix')
+  ports_raw = get2(leaderip, 'ports/' + hostname)[0]
+  phy_ports = []
+  if ports_raw and ports_raw != '_1':
+   phy_ports = ports_raw.split('/')
+  bond_data = get2(leaderip, 'bond', '--prefix')
+  nmports = ""
+  cmports = ""
+  dports = ""
+  has_bond_config = False
+  if bond_data:
+   if len(bond_data) > 0:
+    if bond_data[0] != '_1':
+     has_bond_config = True
+  if not has_bond_config:
+   default_ports_str = ",".join(phy_ports)
+   nmports = default_ports_str
+   cmports = default_ports_str
+   dports = default_ports_str
+  else:
+   for b in bond_data:
+    if isinstance(b, (list, tuple)) and len(b) >= 2:
+     b_key = b[0]
+     b_val = b[1]
+     val_clean = b_val.replace('/', ',') if b_val and b_val != '_1' else ""
+     if 'nmports' in b_key:
+      nmports = val_clean
+     elif 'cmports' in b_key:
+      cmports = val_clean
+     elif 'dports' in b_key:
+      dports = val_clean
+    else:
   try:
      ipaddrsubnet = get('ipaddr/'+hostname)[0].split('/')[1]
   except:
@@ -39,11 +70,11 @@ def getall(*bargs):
   isLeader = False
   if (hostname == leader):
     isLeader = True
-  allhosts.append({"isLeader":isLeader, 'ports':ports,'name':hostname, 'configured':configured, 'alias':alias, 'ipaddr': hostip,'ipaddrsubnet':ipaddrsubnet, 'ntp':ntp, 'tz':tz, 'gw': gw,'dnsname':dnsname, 'dnssearch':dnssearch, 'cluster':mgmt})
-  hostsdict[hostname] = {"isLeader":isLeader, 'ports':ports, 'configured':configured, 'alias':alias, 'ipaddr': hostip, 'ipaddrsubnet':ipaddrsubnet, 'ntp':ntp, 'tz':tz, 'gw': gw, 'dnsname':dnsname, 'dnssearch':dnssearch, 'cluster':mgmt }
+  allhosts.append({"isLeader":isLeader, 'ports':ports,'name':hostname, 'configured':configured, 'alias':alias, 'ipaddr': hostip,'ipaddrsubnet':ipaddrsubnet, 'ntp':ntp, 'tz':tz, 'gw': gw,'dnsname':dnsname, 'dnssearch':dnssearch, 'cluster':mgmt, 'nmports': nmports, 'cmports': cmports, 'dports': dports})
+  hostsdict[hostname] = {"isLeader":isLeader, 'ports':ports, 'configured':configured, 'alias':alias, 'ipaddr': hostip, 'ipaddrsubnet':ipaddrsubnet, 'ntp':ntp, 'tz':tz, 'gw': gw, 'dnsname':dnsname, 'dnssearch':dnssearch, 'cluster':mgmt, 'nmports': nmports, 'cmports': cmports, 'dports': dports }
 
  print(allhosts)
  return hostsdict 
 
-if __name__=='__main__':
+if _name=='main_':
  getall(*sys.argv[1:])
