7245637e0bf6df069be46df8e95623f3d4244c8f
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-12-15 03:50:04 +0200

    Refactor network connection handling in docker_setup.sh
    Refactor network connection setup and cleanup in docker_setup.sh. Remove redundant connection modifications and streamline connection creation logic.

--- docker_setup.sh
+++ docker_setup.sh
@@ -166,10 +166,6 @@ targetcli clearconfig confirm=true
 #nmcli conn delete clusterstub 
 #nmcli conn delete mynode 
 #nmcli conn delete mycluster 
-
-nmcli conn mod mynode connection.interface-name $mynodedev
-nmcli conn mod mycluster connection.interface-name $myclusterdev
-
 nmcli conn up mynode
 nmcli conn delete cmynode
 nmcli conn delete cmycluster
@@ -223,6 +219,10 @@ else
 	
 	else
 		mynode=`nmcli conn show mynode | grep ipv4.addresses | awk '{print $2}'`
+		if [ "$mynodedev" != "bond0" ]; then
+			nmcli conn mod mynode connection.interface-name $mynodedev
+			nmcli conn up mynode
+		fi
 	fi
 
 	caddr=`cat /root/newcaddr`
@@ -234,6 +234,10 @@ else
 		nmcli conn mod mycluster ipv4.addresses $caddr
 	else
 		mycluster=`nmcli conn show mycluster | grep ipv4.addresses | awk '{print $2}'`
+		if [ "$myclusterdev" != "bond0" ]; then
+			echo "DEBUG: Switching mycluster from bond0 to $myclusterdev"
+			nmcli conn mod mycluster connection.interface-name $myclusterdev
+		fi
 	fi
 	myclusterip=`echo $mycluster | awk -F'/' '{print $1}'`
 	mynodeip=`echo $mynode | awk -F'/' '{print $1}'`
@@ -275,44 +279,49 @@ mynodeip=`echo $mynode | awk -F'/' '{print $1}'`
 myip=$mynodeip
 myhostip=$mynodeip
  #/pace/zfsping.py $leaderip $myhost & disown #### it is in refreshdisown
-
-# --- CLEANUP PHASE (replaces lines 226-259) ---
-# Delete ALL temporary connections before creating final ones
-echo "Cleaning up temporary network connections..."
-nmcli conn delete mynode 2>/dev/null
-nmcli conn delete mycluster 2>/dev/null
-nmcli conn delete clusterstub 2>/dev/null
-nmcli conn delete cmynode 2>/dev/null
-nmcli conn delete cmycluster 2>/dev/null
-
-# --- CREATE FINAL CONNECTIONS ---
-# Now create the final connections based on device configuration
-echo "Device config - nodedev=$mynodedev, clusterdev=$myclusterdev"
-
-if [ "$mynodedev" = "$myclusterdev" ]; then
-    # Single device: combine into one connection
-    if [ $isprimary -ne 0 ]; then
-        echo "Primary: creating combined cmynode on $mynodedev with both IPs"
-        nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode ip4 $mycluster
-    else
-        echo "Non-primary: creating cmynode on $mynodedev"
-        nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode
-    fi
+echo $mynodedev | grep $myclusterdev
+if [ $? -eq 0 ];
+then
+case $isconf_prim in 
+nono)
+;;
+noyes)
+;;
+yesno)
+;;
+yesyes)
+;;
+esac
+if [ $isprimary -ne 0 ];
+then
+echo I am prmary
+nmcli conn delete cmynode 
+echo nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode ip4 $mycluster
+nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode ip4 $mycluster
 else
-    # Different devices: create separate connections
-    echo "Creating separate connections for different devices"
-    nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode
-    nmcli conn add con-name cmycluster type bond ifname $myclusterdev ip4 $mycluster
-    if [ $isprimary -ne 0 ]; then
-        echo "Primary: bringing up cmycluster"
-        nmcli conn up cmycluster
-    fi
+echo I am a cluster node 
+nmcli conn delete cmynode 
+nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode
+fi
+else
+case $isconf_prim in 
+nono)
+;;
+noyes)
+;;
+yesno)
+;;
+yesyes)
+;;
+esac
+nmcli conn add con-name cmynode type bond ifname $mynodedev ip4 $mynode
+nmcli conn add con-name cmycluster type bond ifname $myclusterdev ip4 $mycluster
+if [ $isprimary -ne 0 ];
+then
+nmcli conn up cmycluster
 fi
 
-# Bring up final connection(s)
-echo "Bringing up cmynode"
-nmcli conn up cmynode
-
+fi
 echo adding cmynode
 nmcli conn up cmynode
 if [[ $isconf == 'yes' ]];
