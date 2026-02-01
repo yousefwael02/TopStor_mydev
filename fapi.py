7b9b77d08e6ad2e81e3e96e786ec67fa7b723c8d
sdlfkj <dkdjkl@dljkf.com>
2026-02-01 16:07:43 +0200

    fixing

--- fapi.py
+++ fapi.py
@@ -1498,7 +1498,7 @@ def UnixAddUser(data):
 
 
  if int(is_unique_name(data['name']))==1000:
-    logmsg.sendlog('IPnamuqfa','error','system',loggedusers[data['token']]['user'])
+    logmsg.sendlog('Unlin1021uv','error','system',data['name'])
     return data
     
  grps = data.get('groups')
