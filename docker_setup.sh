900ad9493f3ca20939cb642488107c1aed30c431
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2026-02-22 12:09:57 +0200

    docker setup

--- docker_setup.sh
+++ docker_setup.sh
@@ -332,9 +332,11 @@ systemctl start iscsid
 fi
 echo starting docker
 systemctl start docker
+echo Building React App...
+docker run --rm -v /topstorweb:/app -w /app node:18-alpine sh -c "npm install && npm run build"
 rm -rf /root/newipaddr
 rm -rf /root/newcaddr
-docker run --rm --name software  --hostname software  -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $myhostip:80:80 -v /root/gitrepo/httpd.conf:/usr/local/apache2/conf/httpd.conf -v /root/gitrepo:/usr/local/apache2/htdocs/ -itd moataznegm/quickstor:git
+docker run --rm --name software  --hostname software  -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $myhostip:80:80 -v /root/gitrepo/httpd.conf:/usr/local/apache2/conf/httpd.conf -v /root/gitrepo/build_react:/usr/local/apache2/htdocs/ -v /root/gitrepo/plugins:/usr/local/apache2/htdocs/plugins -v /root/gitrepo/dist:/usr/local/apache2/htdocs/dist -v /root/gitrepo/css:/usr/local/apache2/htdocs/css -v /root/gitrepo/img:/usr/local/apache2/htdocs/img -itd moataznegm/quickstor:git
 echo starting intdns
 docker run --rm --name intdns --hostname intdns --net bridge0 -e DNS_DOMAIN=qs.dom -e DNS_IP=10.11.12.7 -e LOG_QUERIES=true -itd --ip 10.11.12.7 -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/dnshosts:/etc/hosts moataznegm/quickstor:dns
 
@@ -609,7 +611,18 @@ then
 	sed -i "s/MYCLUSTERH/$myclusterip/g" $shttpdf
 	sed -i "s/MYCLUSTER/$myclusterip/g" $shttpdf
 	echo running httpd fowrarder as I am not primary
-	docker run --rm --name httpd --hostname shttpd --net bridge0 -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $myclusterip:19999:19999 -p $myclusterip:80:80 -p $myclusterip:443:443 -v $shttpdf:/usr/local/apache2/conf/httpd.conf -v /root/topstorwebetc:/usr/local/apache2/topstorwebetc -v /topstorweb:/usr/local/apache2/htdocs/ -itd moataznegm/quickstor:git
+	docker run --rm --name httpd --hostname shttpd --net bridge0 \
+      -v /etc/localtime:/etc/localtime:ro \
+      -v /root/gitrepo/resolv.conf:/etc/resolv.conf \
+      -p $myclusterip:19999:19999 -p $myclusterip:80:80 -p $myclusterip:443:443 \
+      -v $shttpdf:/usr/local/apache2/conf/httpd.conf \
+      -v /root/topstorwebetc:/usr/local/apache2/topstorwebetc \
+      -v /topstorweb/build_react:/usr/local/apache2/htdocs/ \
+      -v /topstorweb/plugins:/usr/local/apache2/htdocs/plugins \
+      -v /topstorweb/dist:/usr/local/apache2/htdocs/dist \
+      -v /topstorweb/css:/usr/local/apache2/htdocs/css \
+      -v /topstorweb/img:/usr/local/apache2/htdocs/img \
+      -itd moataznegm/quickstor:git
 	docker run -itd --rm --name flask --hostname apisrv -v /etc/localtime:/etc/localtime:ro -v /pace/:/pace -v /pacedata/:/pacedata/ -v /root/gitrepo/resolv.conf:/etc/resolv.conf --net bridge0 -p $myclusterip:5001:5001 -v /TopStor/:/TopStor -v /TopStordata/:/TopStordata moataznegm/quickstor:flask3
 	/TopStor/promserver.sh $myclusterip 
 fi
