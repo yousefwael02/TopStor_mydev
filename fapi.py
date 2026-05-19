00dd48468fd4530533620d9d04ebdbf93b6180af
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2026-05-19 15:25:58 +0300

    Add traceback import to fapi.py

--- fapi.py
+++ fapi.py
@@ -4,7 +4,7 @@ from getversions import getversions
 from functools import wraps
 from copy import deepcopy
 from flask import request, jsonify, render_template, redirect, url_for, g, send_file
-import re, ipaddress
+import re, ipaddress, traceback
 import Hostsconfig
 from Hostconfig import config
 from allphysicalinfo import getall, initallphy
